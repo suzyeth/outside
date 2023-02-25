5b7355e9aacdb69f28c9beff895dc62eb114a190
jinghongleee <Jinghongleee@gmail.com>
2023-02-25 23:34:29 +0000

    Scene1(Incomplete dialogue system)

---
+++ Assets/DialogeSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogeSystem : MonoBehaviour
+{
+    public GameObject DialogeUI;
+    public Text DialogeText;
+    [TextArea(1, 3)] public string[] DialogeTextList;
+    public int currentIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
