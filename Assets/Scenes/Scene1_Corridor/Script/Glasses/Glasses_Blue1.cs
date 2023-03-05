51f231e247b654e3daa08dcfffd161eaadd0b18a
suzyeth <suzw997@126.com>
2023-03-05 21:34:19 +0000

    ziwei su

--- Assets/Scenes/Scene1_Corridor/Script/Glasses/Glasses_Blue1.cs
+++ Assets/Scenes/Scene1_Corridor/Script/Glasses/Glasses_Blue1.cs
@@ -10,9 +10,10 @@ public class Glasses_Blue1 : MonoBehaviour
     public GameObject RedVersion;
 
     public static float count = 0;
-   
+    public GameObject Symbol;
+    public GameObject DoorSymbol;
+    public GameObject LabSymbol;
 
-    
     // Start is called before the first frame update
 
     private void OnMouseDown()
@@ -29,14 +30,24 @@ public class Glasses_Blue1 : MonoBehaviour
         {
             BlueVersion.SetActive(false);
             count = 0;
+            Symbol.SetActive(false);
+            DoorSymbol.SetActive(false);
+            LabSymbol.SetActive(false);
         }
 
-        
+        Symbol.SetActive(true);
+        DoorSymbol.SetActive(true);
+        LabSymbol.SetActive(true);
+
+
     }
 
     void Start()
     {
-        
+        Symbol.SetActive(false);
+
+        DoorSymbol.SetActive(false);
+        LabSymbol.SetActive(false);
     }
 
     // Update is called once per frame
